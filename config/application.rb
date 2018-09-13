0bc9f5d7e6f4502de9b1a12b036828d833830472
Dinesh Chohda <dinesh.skyach@gmail.com>
2018-09-13 15:29:23 +0530

    updates

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module RailsKubeDemo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.active_job.queue_adapter = :sidekiq
+    # config.active_job.queue_adapter = :sidekiq
 
 
     # Settings in config/environments/* take precedence over those specified here.
